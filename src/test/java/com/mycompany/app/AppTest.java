a0adbac37db8532308c464c9965e06c9df634e0c
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-06-18 14:40:41 +0100

    Init

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void whenEmptyString_thenAccept() {
+        assertTrue(App.isPalindrome(""));
+    }
+}
